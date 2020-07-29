5e54580b71a04d8184db2a910d16324358933105
pavan kumar <pavankumargeddapu@gmail.com>
2020-07-29 16:15:10 +0530

    Updates

---
+++ precache-manifest.c8aac4e870aeadf2b0b3eef2f0d92e81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65bf549cf0e218decb32780c21dd5f6a",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "e914ca1c93b057f8f889",
+    "url": "/calculator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ec5ad8ca49fb8e415f7d",
+    "url": "/calculator/static/css/main.ef0edf8d.chunk.css"
+  },
+  {
+    "revision": "e914ca1c93b057f8f889",
+    "url": "/calculator/static/js/2.c26f9c0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.c26f9c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5ad8ca49fb8e415f7d",
+    "url": "/calculator/static/js/main.58808822.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
